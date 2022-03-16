6888251f37dfa68b0769b11693a89de62b312d4f
Quentin Legot <legotquentin@gmail.com>
2022-03-16 01:11:25 +0100

    improve homepage and statusbar

--- Screen/HomeScreen.js
+++ Screen/HomeScreen.js
@@ -1,14 +1,19 @@
 import React, { useContext } from 'react'
-import { Text } from 'react-native'
-import { UsernameContext, TokenContext } from '../Context/Context'
+import { Text, View, TouchableWithoutFeedback} from 'react-native'
+import { UsernameContext } from '../Context/Context'
 
-export default function HomeScreen() {
-  const [username, setUsername] = useContext(UsernameContext)
-  const [token, setToken] = useContext(TokenContext)
+export default function HomeScreen({ navigation }) {
+  const [username] = useContext(UsernameContext)
   return (
-    <>
-      <Text>Welcome !</Text>
-      <Text>You are logged as {username}</Text>
-    </>
+    <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Bienvenue !</Text>
+      <Text>Vous êtes connecté en tant que {username}</Text>
+      <TouchableWithoutFeedback onPress={() => navigation.navigate('taskListStack')}>
+        <Text style={{color: 'blue'}}>Voir votre listes des tâches</Text>
+      </TouchableWithoutFeedback>
+      <TouchableWithoutFeedback onPress={() => navigation.navigate('SignOut')}>
+        <Text style={{color: 'blue', marginTop: 25}}>Se déconnecter</Text>
+      </TouchableWithoutFeedback>
+    </View>
   )
 }
